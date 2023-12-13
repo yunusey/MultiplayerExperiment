7fc4ea1fdb1d6e94635cc6b2e271ceb78a20ead9
yunusey <ynsmrycl2007@gmail.com>
2023-12-12 21:16:14 -0500

    Add messaging feature

--- Globals.gd
+++ Globals.gd
@@ -1,3 +1,35 @@
 extends Node
 
 var players: Dictionary = {}
+var username_color: Color
+# This is the color of the username of the local player.
+# It will be set by Interface once the program starts.
+
+class Message:
+	const FORMATTED_MESSAGE = "[color=#{color}][i]{username}[/i][/color]: {message}"
+
+	var id: int
+	var username: String
+	var message: String
+	var color: Color
+
+	static func from(in_dict: Dictionary):
+		var new_message = Message.new()
+		new_message.id = in_dict["id"]
+		new_message.username = in_dict["username"]
+		new_message.message = in_dict["message"]
+		new_message.color = in_dict["color"]
+		
+		return new_message
+	
+	func _to_string():
+		return str(id, " ", username, " ", message)
+	
+	func get_bbcode() -> String:
+		return FORMATTED_MESSAGE.format({
+			"color": color.to_html(false),
+			"username": username,
+			"message": message
+		})
+
+var messages: Array[Message] = []
